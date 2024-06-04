b65244a5f21124092af27374a36674dba420c0e0
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-06-04 21:16:26 +0100

    Initial Commit

---
+++ GoMobileTest/GoMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoMobileTestApp.swift
+//  GoMobileTest
+//
+//  Created by Ayodeji Osasona on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
